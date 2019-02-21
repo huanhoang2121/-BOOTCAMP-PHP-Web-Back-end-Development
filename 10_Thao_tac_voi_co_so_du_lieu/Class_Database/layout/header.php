82f797c7df1130749efd2f72e1aaee119a1be5b0
Hoàng Văn Huấn <38126411+huanhoang2121@users.noreply.github.com>
2019-02-21 11:38:24 +0700

    Add class_database

---
+++ 10_Thao_tac_voi_co_so_du_lieu/Class_Database/layout/header.php
@@ -0,0 +1,8 @@
+<div class="scrollmenu">
+	<a href="#">Home</a>
+	<a href="#">Book</a>
+	<a href="#">Reader</a>
+	<a href="#">Borrow Books</a>
+	<a href="#">Author</a>
+	<a href="./displaycategory.php">Categories</a>
+</div>
