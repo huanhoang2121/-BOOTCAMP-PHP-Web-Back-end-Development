7cba7a1aad73e62c5c1bb597a79419f0d12f22c0
Hoàng Văn Huấn <38126411+huanhoang2121@users.noreply.github.com>
2019-02-22 10:50:31 +0700

    update quan li san pham

--- MVC/ProductManager/view/delete.php
+++ MVC/ProductManager/view/delete.php
@@ -1,4 +1,4 @@
-<h1>Bạn chắc chắn muốn xóa khách hàng này?</h1>
+<h1>Bạn chắc chắn muốn xóa sản phẩm này?</h1>
 <h3><?php echo $product->name; ?></h3>
 <form action="./index.php?page=delete" method="post">
   <input type="hidden" name="id" value="<?php echo $product->id; ?>"/>
